4546a8694538677cf68021485faeb38cb11457b8
Malone1996 <mayaobo1996@qq.com>
2019-11-22 13:16:31 +0800

    加入自动导入数据库功能

--- app/src/main/java/com/example/hasee/expandablelistviewdemo/MainActivity.java
+++ app/src/main/java/com/example/hasee/expandablelistviewdemo/MainActivity.java
@@ -6,14 +6,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
     private Button mBtnSingleChoice;
     private Button mBtnFillBlank;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String DB_PATH="/data/data/com.example.hasee.expandablelistviewdemo/databases/";
+        String DB_NAME="question.db";
+
+        //判断数据库是否存在，不存在则创建数据库
+        if ((new File(DB_PATH+DB_NAME).exists())==false){
+            File dir=new File(DB_PATH);
+            if (!dir.exists()){
+                dir.mkdir();
+            }
+
+            try {
+                InputStream is=getBaseContext().getAssets().open(DB_NAME);
+                OutputStream os=new FileOutputStream(DB_PATH+DB_NAME);
+                byte[] buffer =new byte[1024];
+                int length;
+
+                while ((length=is.read(buffer))>0){
+                    os.write(buffer,0,length);
+                }
+
+                os.flush();
+                os.close();
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         mBtnSingleChoice=findViewById(R.id.btn_singlechoice);
         mBtnSingleChoice.setOnClickListener(new View.OnClickListener() {
             @Override
